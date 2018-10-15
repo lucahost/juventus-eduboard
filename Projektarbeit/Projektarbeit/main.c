cb4bdf501b1c9ad4c98f23d1bf04305edc362312
xXx-DoMoDo-xXx <43957849+xXx-DoMoDo-xXx@users.noreply.github.com>
2018-10-15 21:32:12 +0200

    0.2
    Ein paar kleine Optimierungen. SETLED Funktion optimiert. SW2 UART Fehler behoben. SW3 noch leer, wird noch nachgereicht ;)

--- Projektarbeit/Projektarbeit/main.c
+++ Projektarbeit/Projektarbeit/main.c
@@ -1,32 +1,31 @@
 #include <avr/io.h>
 #include <stdint.h>
+#include <util/delay.h>
 
 volatile uint32_t Time;
 volatile uint8_t anzahl = 0;
-volatile uint16_t ADC_result;
+volatile uint8_t ADC_result = 0x00;
+volatile char writeledbyte = 0x00;								// variable nullsetzen
 
 volatile uint8_t SW1_actual;
-
 volatile uint8_t SW2_actual;
-
 volatile uint8_t SW3_actual;
 
 volatile uint16_t Realer_Wert;
-volatile char Display_Wert[6];
+char Display_Wert[7] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00};
 
-
-char SETLED (writeledbyte)
+void SETLED (int value)
 {
-	writeledbyte = ~writeledbyte;									// Variable wird negiert
-	PORTF.OUT = (PORTF.OUT & 0xF0);									// PORTF wird mit UND Maskiert
-	PORTF.OUT = ((PORTF.OUT & 0x0F) | (writeledbyte & 0x0F));		// PORTF wird mit UND Maskiert, nur F wird beschrieben. Variable writeledbyte ebenfalls mit UND Maskiert, low nibble wird ins PORTF übertragen
-	PORTE.OUT = (PORTE.OUT & 0xF0);									// PORTE wird mit UND Maskiert
-	PORTE.OUT = ((PORTE.OUT & 0x0F) | ((writeledbyte & 0xF0) >> 4));// PORTF wird mit UND Maskiert, nur F wird beschrieben. Variable writeledbyte ebenfalls mit UND Maskiert high nibble und dann wird die high nibble um 4 mal nach rechts geshiftet und dann ins PORTF übertragen
+	value = ~value;									// Variable wird negiert
+
+	PORTF.OUT |= 0x0F;
+	PORTF.OUT &= (value | 0xF0);
+		
+	PORTE.OUT |= 0x0F;
+	PORTE.OUT &= ((value | 0x0F) >> 4);	
 }
 
 int main(void)
-
-
 {
 	/******* Start A/D Wandler Initialisierung ********/
 
@@ -60,78 +59,67 @@ int main(void)
 	PORTF.DIR = 0x0F;												// PORTF Port 0 bis 3 auf Output, 4 bis 7 auf Eingang
 	PORTE.DIR = 0x0F;												// PORTE Port 0 bis 3 auf Output, 4 bis 7 auf Eingang
 
-	volatile char writeledbyte = 0x00;								// variable nullsetzen
-	SETLED (writeledbyte);											// LEDs setzen
+	
+	SETLED (ADC_result);														// LEDs setzen
 
 	/******* Ende Ports Initialisierung ********/
 	
 	while(1)
 	{	
-		SW1_actual = ((~PORTF.IN >> 4) & 0b00000001);				// SW1 Status auslesen
-		SW2_actual = ((~PORTF.IN >> 5) & 0b00000001);				// SW2 Status auslesen
-		SW3_actual = ((~PORTF.IN >> 6) & 0b00000001);				// SW3 Status auslesen
+		SW1_actual = ((~PORTF.IN >> 4) & 0x01);				// SW1 Status auslesen
+		SW2_actual = ((~PORTF.IN >> 5) & 0x01);				// SW2 Status auslesen
+		SW3_actual = ((~PORTF.IN >> 6) & 0x01);				// SW3 Status auslesen
 		
 		if (SW1_actual)												// Taste 1 gedrueckt?
 		{
 			ADCA.CH0.CTRL |= ADC_CH_START_bm;						// A/D Wandler starten
 			while (!ADCA.CH0.INTFLAGS);								// Warte bis Wandlung abgeschlossen
 			ADC_result= ADCA.CH0.RES;								// Wandlung Resultat ins ADC_result eintragen
-			writeledbyte = ADC_result;								// ADC_result ins writeledbyte eintragen
 
-			SETLED (writeledbyte);									// A/D Wandlung Resultat wird dann optisch auf LED übertragen und es zeigt nun Binäre Wert an.
+			SETLED (ADC_result);											// A/D Wandlung Resultat wird dann optisch auf LED übertragen und es zeigt nun Binäre Wert an.
 			
 			while (SW1_actual)										// bleibt sollange in die Schleife, bis die Taste 1 nicht mehr gedrueckt ist.
 			{
-				SW1_actual = ((~PORTF.IN >> 4) & 0b00000001);		// SW1 Status auslesen
+				SW1_actual = ((~PORTF.IN >> 4) & 0x01);		// SW1 Status auslesen
 			}
 		}				
 			 
 	
 		if (SW2_actual)
-		{		
+		{	
 			Realer_Wert = ADC_result * 39;							// wie man auf *39 kommt, AD Resultat ist immer max 255. Also 1V / 255 = 0.003921
 			Display_Wert[0] = (Realer_Wert / 1000) | 0x30;
 			Display_Wert[1] = ((Realer_Wert % 1000) / 100) | 0x30;
 			Display_Wert[2] = ((Realer_Wert % 1000) % 100 / 10) | 0x30;
-//			Display_Wert[3] = 0x6D;									// m
-//			Display_Wert[4] = 0x56;									// V
-			Display_Wert[3] = 0x0A;									// Line Feed
-			Display_Wert[4] = 0x0D;									// Carriage Return			
+			Display_Wert[3] = 0x6D;									// m
+			Display_Wert[4] = 0x56;									// V
+			Display_Wert[5] = 0x0D;									// Carriage Return			
+			Display_Wert[6] = 0x0A;									// Line Feed
 		
-			for (uint8_t i = 0; i <= 3; i++)
+			for (int j = 0; j < sizeof(Display_Wert); j++)
 			{
-				USARTC0_DATA = Display_Wert[i];						// sendet das Zeichen RX_Char
-				while (!( USARTC0_STATUS & USART_TXCIF_bm));		// wartet bis Zeichen gesendet	
+				while (!(USARTC0_STATUS & USART_DREIF_bm));			// wartet bis Zeichen gesendet
+				USARTC0_DATA = Display_Wert[j];						// sendet das Zeichen TX_Char	
 			}
 			
 			while (SW2_actual)										// bleibt sollange in die Schleife, bis die Taste 2 nicht mehr gedrueckt ist.
 			{
-				SW2_actual = ((~PORTF.IN >> 5) & 0b00000001);		// SW2 Status auslesen
+				SW2_actual = ((~PORTF.IN >> 5) & 0x01);		// SW2 Status auslesen
 			}
 		}
 
 		if (SW3_actual)
 		{
-			Realer_Wert = ADC_result * 39;							// wie man auf *39 kommt, AD Resultat ist immer max 255. Also 1V / 255 = 0.003921
-			Display_Wert[0] = (Realer_Wert / 1000) | 0x30;
-			Display_Wert[1] = ((Realer_Wert % 1000) / 100) | 0x30;
-			Display_Wert[2] = ((Realer_Wert % 1000) % 100 / 10) | 0x30;
-//			Display_Wert[3] = 0x6D;									// m
-//			Display_Wert[4] = 0x56;									// V
-			Display_Wert[3] = 0x0A;									// Line Feed
-			Display_Wert[4] = 0x0D;									// Carriage Return			
-		
-			for (uint8_t i = 0; i <= 3; i++)
-			{
-				USARTC0_DATA = Display_Wert[i];						// sendet das Zeichen RX_Char
-				while (!( USARTC0_STATUS & USART_TXCIF_bm));		// wartet bis Zeichen gesendet	
-			}			
+
+
+
+
 			
+
 			while (SW3_actual)										// bleibt sollange in die Schleife, bis die Taste 3 nicht mehr gedrueckt ist.
 			{
-				SW3_actual = ((~PORTF.IN >> 6) & 0b00000001);		// SW3 Status auslesen
-			}
-			
+				SW3_actual = ((~PORTF.IN >> 6) & 0x01);				// SW3 Status auslesen
+			}		
 		}		
 		
 //		Time=0;
