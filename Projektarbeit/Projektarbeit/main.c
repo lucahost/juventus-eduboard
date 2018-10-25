9b23ee7c68da37c3665e4135d0975d25d6f98c89
xXx-DoMoDo-xXx <43957849+xXx-DoMoDo-xXx@users.noreply.github.com>
2018-10-25 22:32:12 +0200

    SW3 Taste berrechnung erweitert

--- Projektarbeit/Projektarbeit/main.c
+++ Projektarbeit/Projektarbeit/main.c
@@ -1,3 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////////
+//
+//		Autor: Dominic WÃ¼thrich
+//		Datum: 25.10.2018
+//		Projekt: EduBoard mit AD und Serielle Ãœbertragung
+//
+////////////////////////////////////////////////////////////////////////////////////
+
 #include <avr/io.h>
 #include <stdint.h>
 #include <util/delay.h>
@@ -56,14 +64,12 @@ int main(void)
 	/******* Ende UART Initialisierung ********/
 
 	/******* Start Ports Initialisierung ********/
-	PORTF.DIR = 0x0F;												// PORTF Port 0 bis 3 auf Output, 4 bis 7 auf Eingang
 	PORTE.DIR = 0x0F;												// PORTE Port 0 bis 3 auf Output, 4 bis 7 auf Eingang
-
-	
-	SETLED (ADC_result);														// LEDs setzen
-
+	PORTF.DIR = 0x0F;												// PORTF Port 0 bis 3 auf Output, 4 bis 7 auf Eingang
 	/******* Ende Ports Initialisierung ********/
 	
+	SETLED (ADC_result);											// LEDs setzen
+
 	while(1)
 	{	
 		SW1_actual = ((~PORTF.IN >> 4) & 0x01);				// SW1 Status auslesen
@@ -76,7 +82,7 @@ int main(void)
 			while (!ADCA.CH0.INTFLAGS);								// Warte bis Wandlung abgeschlossen
 			ADC_result= ADCA.CH0.RES;								// Wandlung Resultat ins ADC_result eintragen
 
-			SETLED (ADC_result);											// A/D Wandlung Resultat wird dann optisch auf LED übertragen und es zeigt nun Binäre Wert an.
+			SETLED (ADC_result);											// A/D Wandlung Resultat wird dann optisch auf LED ï¿½bertragen und es zeigt nun Binï¿½re Wert an.
 			
 			while (SW1_actual)										// bleibt sollange in die Schleife, bis die Taste 1 nicht mehr gedrueckt ist.
 			{
@@ -109,12 +115,23 @@ int main(void)
 		}
 
 		if (SW3_actual)
-		{
+		{	// mit 2 Stellen nach Komma
+			Realer_Wert = ADC_result * 39;							// Hier z.b. Resultat 9945,  wie man auf *39 kommt, AD Resultat ist immer max 255. Also 1V / 255 = 0.003921
+			Display_Wert[0] = (Realer_Wert / 10000) | 0x30;				// Vom Beispiel 10000 nach 1 V
+			Display_Wert[1] = 0x2C;										// , (Komma)
+			Display_Wert[2] = ((Realer_Wert % 10000) / 1000) | 0x30;		// vom beispiel 9945 Modulo 10000 gibt 9945 rest, dann geteilt durch 1000 gibts 9
+			Display_Wert[3] = ((Realer_Wert % 1000) / 100) | 0x30;		// vom beispiel 9945 Modulo 1000 gibt 945 rest, dann geteilt durch 100 gibts 9
+//			Display_Wert[2] = ((Realer_Wert % 100) / 10) | 0x30;			// vom beispiel 9945 Modulo 100 gibt 45 rest, dann geteilt durch 10 gibts 4
+//			Display_Wert[2] = ((Realer_Wert % 10) | 0x30;				// vom beispiel 9945 Modulo 10 gibt 5 rest, gibts 9
+			Display_Wert[4] = 0x56;										// V
+			Display_Wert[5] = 0x0D;										// Carriage Return			
+			Display_Wert[6] = 0x0A;										// Line Feed
 
-
-
-
-			
+			for (int j = 0; j < sizeof(Display_Wert); j++)
+			{
+				while (!(USARTC0_STATUS & USART_DREIF_bm));			// wartet bis Zeichen gesendet
+				USARTC0_DATA = Display_Wert[j];						// sendet das Zeichen TX_Char	
+			}
 
 			while (SW3_actual)										// bleibt sollange in die Schleife, bis die Taste 3 nicht mehr gedrueckt ist.
 			{
